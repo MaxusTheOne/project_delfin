0b7c86c7f23c82e4a1fa9dfe7c1b6b8454cada7b
fili0727 <123998888+fili0727@users.noreply.github.com>
2023-05-09 14:11:22 +0200

    Update structure

--- js/rest.js
+++ js/rest.js
@@ -4,22 +4,22 @@ const exampleUserStructure = {
   subscription: "senior", //young, senior, retired, passive
   discipline: "butterfly", //butterfly, crawl, backCrawl and breastStroke
   age: 33,
-  results: [],
   coach: "",
   firstName: "Taylor",
   lastName: "Swift",
   debt: 900,
   gender: "female",
-  picture: "https://pbs.twimg.com/media/FTDgrd9X0AMLEuQ?format=jpg&name=medium",
+  image: "https://pbs.twimg.com/media/FTDgrd9X0AMLEuQ?format=jpg&name=medium",
 };
 
 const exampleResultStructure = {
   meetName: "The swifties swim",
-  date: new Date("2015-03-25"),
+  date: "2015-03-25",
   place: 1,
-  participant: "",
+  participantId: "",
   discipline: "butterfly", //butterfly, crawl, backCrawl and breastStroke
-  time: 125, //formatted in seconds
+  time: 125, //formatted in seconds'
 };
 
-const url = "https://delfindaba-16acc-default-rtdb.europe-west1.firebasedatabase.app/";
+const url =
+  "https://delfindaba-16acc-default-rtdb.europe-west1.firebasedatabase.app/";
